31ed64437971db99c8d8bcb7736347d4f9dd4bf0
Daryl Carr <darylcarr@live.co.uk>
2017-09-06 23:18:21 +0100

    [LAST] Returns undefined if list is not array or string

--- spec/last.spec.js
+++ spec/last.spec.js
@@ -5,4 +5,11 @@ describe('first()', () => {
     it('should be a function', () => {
         expect(last).to.be.a('function');
     });
+    it('returns undefined if list is not array or string', () => {
+        const expected = undefined;
+        expect(last(12)).to.eql(expected);
+        expect(last(null)).to.eql(expected);
+        expect(last(true)).to.eql(expected);
+        expect(last(false)).to.eql(expected);
+    });
 });
