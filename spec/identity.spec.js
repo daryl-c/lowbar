c0326dfdc526284a4943a11a8a70915eef416eaf
Daryl Carr <darylcarr@live.co.uk>
2017-09-06 23:02:11 +0100

    [IDENTITY] Returns correctly for non-primitives

--- spec/identity.spec.js
+++ spec/identity.spec.js
@@ -10,4 +10,10 @@ describe('identity()', () => {
         expect(identity('a')).to.equal('a');
         expect(identity(true)).to.equal(true);
     });
+    it('returns same argument it is called with for non-primitives', () => {
+        var arr = [1, 2, 3];
+        var obj = {a: 1, b: 2};
+        expect(identity(arr)).to.equal(arr);
+        expect(identity(obj)).to.equal(obj);
+    });
 });
