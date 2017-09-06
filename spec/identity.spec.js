2fa85b7e321cebe84adff1dad82e6f5ced7c3ba2
Daryl Carr <darylcarr@live.co.uk>
2017-09-06 23:01:36 +0100

    [IDENTITY] Returns correct value for primitives

--- spec/identity.spec.js
+++ spec/identity.spec.js
@@ -5,4 +5,9 @@ describe('identity()', () => {
     it('should be a function', () => {
         expect(identity).to.be.a('function');
     });
+    it('returns same argument it is called with for primitives', () => {
+        expect(identity(1)).to.equal(1);
+        expect(identity('a')).to.equal('a');
+        expect(identity(true)).to.equal(true);
+    });
 });
