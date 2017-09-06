debfa4b29e2363e881c1b4dcd6a46889fe897055
Daryl Carr <darylcarr@live.co.uk>
2017-09-06 23:07:54 +0100

    [FIRST] Returns empty array if input is not array

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -5,4 +5,12 @@ describe('first()', () => {
     it('should be a function', () => {
         expect(first).to.be.a('function');
     });
+    it('returns an empty array if input is not array', () => {
+        const expected = [];
+        expect(first(12)).to.eql(expected);
+        expect(first(null)).to.eql(expected);
+        expect(first(true)).to.eql(expected);
+        expect(first(false)).to.eql(expected);
+        expect(first('abc')).to.eql(expected);
+    });
 });
