bffb62d08341f703703bbbfca04d56e1620529a5
Daryl Carr <darylcarr@live.co.uk>
2017-09-07 22:18:56 +0100

    [EACH] Call iteratee for every item in an array

--- spec/each.spec.js
+++ spec/each.spec.js
@@ -1,8 +1,18 @@
 const {each} = require('../lib');
 const {expect} = require('chai');
+const {spy} = require('sinon');
 
 describe('first()', () => {
     it('should be a function', () => {
         expect(each).to.be.a('function');
+    });
+    describe('arrays', () => {
+        it('calls the iteratee for every item in an array', () => {
+            const fakeFn = spy();
+            each([1, 2, 3, 4, 5], fakeFn);
+            const expected = 5;
+            const actual = fakeFn.callCount;
+            expect(actual).to.equal(expected);
+        });
     });
 });
