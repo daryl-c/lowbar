8bd96f2382c08fdf32d95b6133db3cb0d56223a1
Daryl Carr <darylcarr@live.co.uk>
2017-09-07 22:26:48 +0100

    [EACH] Invokes iteratee with item, index and list

--- spec/each.spec.js
+++ spec/each.spec.js
@@ -14,5 +14,15 @@ describe('first()', () => {
             const actual = fakeFn.callCount;
             expect(actual).to.equal(expected);
         });
+        it('invokes iteratee with the item, it\'s index and the array', () => {
+            const fakeFn = spy();
+            const list = [1, 2, 3, 4, 5];
+            each(list, fakeFn);
+            list.forEach((item, i, arr) => {
+                const actual = fakeFn.calledWith(item, i, arr);
+                const expected = true;
+                expect(actual).to.equal(expected);
+            });
+        });
     });
 });
