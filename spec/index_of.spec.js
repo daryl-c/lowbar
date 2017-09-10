db9d9e921e429b9b0a25b699b72b068cf8b87683
Daryl Carr <darylcarr@live.co.uk>
2017-09-10 18:23:31 +0100

    [INDEX_OF] Return the correct index if given value is present in array

--- spec/index_of.spec.js
+++ spec/index_of.spec.js
@@ -5,4 +5,10 @@ describe('indexOf()', () => {
     it('should be a function', () => {
         expect(indexOf).to.be.a('function');
     });
+    it('should return the correct index if given value is present', () => {
+        const arr = [1, 2, 3, 4, 5];
+        const expected = 3;
+        const actual = indexOf(arr, 4);
+        expect(actual).to.equal(expected);
+    });
 });
