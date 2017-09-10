cbd3b1a98bd9aa44358f4e53bbb315403e2d6a42
Daryl Carr <darylcarr@live.co.uk>
2017-09-10 20:53:21 +0100

    [FILTER] Invokes predicate correctly for arrays

--- spec/lib/filter.spec.js
+++ spec/lib/filter.spec.js
@@ -1,5 +1,6 @@
 const {filter} = require('../../lib');
 const {expect} = require('chai');
+const sinon = require('sinon');
 
 describe('filter()', () => {
     it('should be a function', () => {
@@ -11,5 +12,21 @@ describe('filter()', () => {
         const actual = filter(input, allTrue);
         expect(actual).to.be.an('array');
         expect(actual).to.not.equal(input);
+    });
+    describe('arrays', () => {
+        it('invokes the predicate for every item in array', () => {
+            const input = ['a', 'b', 'c'];
+            const spy = sinon.spy();
+            filter(input, spy);
+            expect(spy.callCount).to.equal(input.length);
+        });
+        it('invokes predicate with each item, ', () => {
+            const input = ['a', 'b', 'c'];
+            const spy = sinon.spy();
+            filter(input, spy);
+            input.forEach((letter, i) => {
+                expect(spy.calledWith(letter, i, input)).to.be.true;
+            });
+        });
     });
 });
