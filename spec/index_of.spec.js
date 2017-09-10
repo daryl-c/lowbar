31cc567020041eb36d5e586df6f5d46cfd8e9834
Daryl Carr <darylcarr@live.co.uk>
2017-09-10 18:25:47 +0100

    [INDEX_OF] Return the first index if given value is present multiple times in array

--- spec/index_of.spec.js
+++ spec/index_of.spec.js
@@ -11,4 +11,10 @@ describe('indexOf()', () => {
         const actual = indexOf(arr, 4);
         expect(actual).to.equal(expected);
     });
+    it('should return the first index if given value is present in multiple places', () => {
+        const arr = [1, 2, 3, 2, 5];
+        const expected = 1;
+        const actual = indexOf(arr, 2);
+        expect(actual).to.equal(expected);
+    });
 });
