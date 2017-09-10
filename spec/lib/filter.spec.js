bc6d03a156a2a77d4f9a7fc3a6a5ef3953ac95c1
Daryl Carr <darylcarr@live.co.uk>
2017-09-10 20:45:03 +0100

    [FILTER] Returns a new array

--- spec/lib/filter.spec.js
+++ spec/lib/filter.spec.js
@@ -5,4 +5,11 @@ describe('filter()', () => {
     it('should be a function', () => {
         expect(filter).to.be.a('function');
     });
+    it('returns a new array', () => {
+        const input = [1, 2, 3];
+        const allTrue = () => true;
+        const actual = filter(input, allTrue);
+        expect(actual).to.be.an('array');
+        expect(actual).to.not.equal(input);
+    });
 });
